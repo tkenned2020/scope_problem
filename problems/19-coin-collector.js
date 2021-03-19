6979e675b59611473948431f909177650c714109
tkenned2020 <tkenned2020@outlook.com>
2021-03-19 15:17:31 -0400

    complete problem 19

--- problems/19-coin-collector.js
+++ problems/19-coin-collector.js
@@ -9,23 +9,47 @@ Once the function returned by coinCollector has the numCoins required it will
 return an array of the coins it has gathered.
 
 Example 1:
+  let oneCoin = coinCollector(1); // returns a function console.log(oneCoin(10)); // prints [10] Example 2:
+  let twoCoins = coinCollector(2); // returns a function twoCoins(25); // returns a function console.log(twoCoins(10)); // prints [25, 10] Example 3:
+  let threeCoins = coinCollector(3); // returns a function threeCoins(25); // returns a function threeCoins(5); // returns a function console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
+
+***********************************************************************/
+
+// your code here
+
+let coinCollector = (numCoins) => {
+  if(numCoins <= 0){
+    return null;
+  }
+  let totalCoins = [];
+return (...coins) => {
+  for(let i = 0; i < coins.length; i++){
+    let coin = coins[i];
+    if(totalCoins.length < numCoins){
+      totalCoins.push(coin)
+    }
+
+  }
+return totalCoins
+}
+}
+
+
+// Example 1:
   let oneCoin = coinCollector(1); // returns a function
   console.log(oneCoin(10)); // prints [10]
 
-Example 2:
+// Example 2:
   let twoCoins = coinCollector(2); // returns a function
   twoCoins(25); // returns a function
   console.log(twoCoins(10)); // prints [25, 10]
 
-Example 3:
+// Example 3:
   let threeCoins = coinCollector(3); // returns a function
   threeCoins(25); // returns a function
   threeCoins(5); // returns a function
   console.log(threeCoins(10)); // prints [ 25, 5, 10 ]
 
-***********************************************************************/
-
-// your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
