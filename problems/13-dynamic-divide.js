ec9826033500d4270c3d52fe473fcea6cd9587b9
tkenned2020 <tkenned2020@outlook.com>
2021-03-19 14:30:59 -0400

    problems 12 13 15 solved

--- problems/13-dynamic-divide.js
+++ problems/13-dynamic-divide.js
@@ -20,6 +20,22 @@ divideByFive(50); // returns 10
 
 // your code here
 
+let dynamicDivide = (divisor) => {
+
+  return function (dividedBy){
+      return dividedBy / divisor
+  }
+}
+
+const halfer = dynamicDivide(2); // returns a function
+console.log(halfer(20)); // returns 10
+
+const divideByThree = dynamicDivide(3);
+console.log(divideByThree(30)); // returns 10
+
+const  divideByFive = dynamicDivide(5);
+console.log(divideByFive(50)); // returns 10
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
--- problems/15-closure-dynamic-multi.js
+++ problems/15-closure-dynamic-multi.js
@@ -20,6 +20,24 @@ multiplyByFive(5); // returns 25
 
 // your code here!
 
+let dynamicMultiply = (num) => {
+
+  return function (num1){
+    return num * num1;
+  }
+
+}
+
+
+const doubler = dynamicMultiply(2); // returns a functions
+console.log(doubler(5)); // returns 10
+
+const tripler = dynamicMultiply(3);
+console.log(tripler(5)); // returns 15
+
+const multiplyByFive = dynamicMultiply(5);
+console.log(multiplyByFive(5)); // returns 25
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
